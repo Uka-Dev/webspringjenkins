d8ea38d8084c7a34470301745846428edb4f911e
Uka-Dev <borjasl2012@gmail.com>
2024-12-05 16:27:48 +0100

    Nueva subida

---
+++ webspring/src/main/java/com/arquitecturajava/webspring/Persona.java
@@ -0,0 +1,24 @@
+package com.arquitecturajava.webspring;
+
+public class Persona {
+	
+	private String persona;
+
+	public Persona() {
+		super();
+	}
+	
+	public Persona(String persona) {
+		super();
+		this.persona = persona;
+	}
+
+	public String getPersona() {
+		return persona;
+	}
+
+	public void setPersona(String persona) {
+		this.persona = persona;
+	}
+
+}
